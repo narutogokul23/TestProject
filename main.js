c49e2b9eafa7503d8101f9e63c15322ae72203dd
narutogokul23 <karthickgokul227@gmail.com>
2023-01-14 21:44:30 +0530

    new changes by gokul

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let num1 = 200;
 let num2 = 20;
 
+let numbers = '8974382941'
+let array = numbers.split('')
+let filtered = array.filter((e)=> e >= '5')
+console.log(filtered)
+
 let  multiply = num1 * num2;
 
 console.log(multiply);
